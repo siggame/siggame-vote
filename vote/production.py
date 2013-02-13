d41f3364dd5f18fa5dfac2547cad4ff3670a8f31
Michael Wisely <michaelwisely@gmail.com>
2013-02-13 10:33:01 -0600

    Added a production django tool to buildout and setup postgres

--- vote/production.py
+++ vote/production.py
@@ -1,4 +1,14 @@
-
 from vote.settings import *
+from secret_settings import *
 
 SIDE_ID = 2
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vote',
+        'USER': 'vote_user',
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': 'localhost'
+    }
+}
