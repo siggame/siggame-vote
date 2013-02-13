a0abb14e16581f9a1af4314bbcb2418299288d01
Michael Wisely <michaelwisely@gmail.com>
2013-02-13 11:59:49 -0600

    Added a logout link

--- vote/accounts/urls.py
+++ vote/accounts/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.views import logout
 
 from .views import LoginStageOneView, LoginStageTwoView
 
 
 urlpatterns = patterns(
     '',
-
     url('^login/$', LoginStageOneView.as_view(), name="login"),
     url('^login/next/', LoginStageTwoView.as_view(), name="login2"),
+
+    url('^logout/$', logout, {"next_page": "/"}, name="logout"),
+
 )
