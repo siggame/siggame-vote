16c9aed756308645ac2a72be3fcfaf7cb8d09b8f
Michael Wisely <michaelwisely@gmail.com>
2013-02-14 10:00:04 -0600

    Fixed UI. It was a type issue.

--- vote/static/js/votelist.js
+++ vote/static/js/votelist.js
@@ -10,6 +10,8 @@ $(function() {
 
   $("#submit-id-submit").click(function() {
     var data_array = $("#sortable").sortable("toArray", {attribute: 'sortable_choice'});
+    console.log(data_array);
+    data_array = _.map(data_array, function(x){return parseInt(x)});
     var data_str = JSON.stringify(data_array);
     console.log(data_array);
     $("#id_data").val(data_str);
