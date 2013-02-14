f46b58d581028c0a8d6ce2e4b065392954b55b7a
Michael Wisely <michaelwisely@gmail.com>
2013-02-14 10:59:08 -0600

    Fixed UI to save the strings, not id numbers

--- vote/static/js/votelist.js
+++ vote/static/js/votelist.js
@@ -2,7 +2,7 @@ $(function() {
   var options = $.parseJSON($("#template").text());
 
   for (var i = 0; i < options.length; i++) {
-    $("#sortable").append('<li sortable_choice="' + i + '" class="ui-state-default">' + options[i] + '</li>');
+    $("#sortable").append('<li sortable_choice="' + options[i] + '" class="ui-state-default">' + options[i] + '</li>');
   }
 
   $( "#sortable" ).sortable();
@@ -10,8 +10,6 @@ $(function() {
 
   $("#submit-id-submit").click(function() {
     var data_array = $("#sortable").sortable("toArray", {attribute: 'sortable_choice'});
-    console.log(data_array);
-    data_array = _.map(data_array, function(x){return parseInt(x)});
     var data_str = JSON.stringify(data_array);
     console.log(data_array);
     $("#id_data").val(data_str);
