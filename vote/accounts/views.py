1c47c1332821a02a8ccc0a94bb321f8def7e402e
Michael Wisely <michaelwisely@gmail.com>
2013-02-14 11:33:59 -0600

    Added user scope

--- vote/accounts/views.py
+++ vote/accounts/views.py
@@ -18,7 +18,8 @@ class LoginStageOneView(RedirectView):
         # is happening.
         state = State.objects.create()
         query_string = urlencode({'client_id': settings.GITHUB_CLIENT_ID,
-                                  'state': state})
+                                  'state': state,
+                                  'scope': 'user'})
 
         return 'https://github.com/login/oauth/authorize?' + query_string
 
