57301d214dbca3ab5187080c7b97b215e697cf3c
Michael Wisely <michaelwisely@gmail.com>
2013-09-19 03:25:51 +0000

    Change GitHub permission scope to user:email

--- vote/accounts/views.py
+++ vote/accounts/views.py
@@ -19,7 +19,7 @@ class LoginStageOneView(RedirectView):
         state = State.objects.create()
         query_string = urlencode({'client_id': settings.GITHUB_CLIENT_ID,
                                   'state': state,
-                                  'scope': 'user'})
+                                  'scope': 'user:email'})
 
         return 'https://github.com/login/oauth/authorize?' + query_string
 
